912f8dedefa462f48e76a63d577f558f744f55a8
ÄleksRomanov <kosmonavt-vavilon@mail.ru>
2020-01-31 16:42:11 +0700

    3. DOM Личный проект: больше деталей (часть 1). Вывод картинок на интерфейс

--- js/main.js
+++ js/main.js
@@ -1,87 +1,90 @@
 'use strict';
 
-var NUMBERS_OBJECTS = 25;
-var MIN_LIKES = 15;
-var MAX_LIKES = 200;
-var MIN_COMMENTS = 1;
-var MAX_COMMENTS = 8;
-// var AVATARS_MAX = 6;
-var DESCRIPTION_STRING = 1;
-
-/* ------------------------------------------------------------------------------------------------------ */
-
-var photosArray = [];
-
-var COMMENTS = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-
-var MESSAGE = [
-  'Тестим новую камеру!',
-  'Затусили с друзьями на море',
-  'Как же круто тут кормят',
-  'Отдыхаем...',
-  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
-  'Вот это тачка!',
-];
-
-/* ------------------------------------------------------------------------------------------------------ */
-/* Функция генерации случайного числа */
-var getRandomNumber = function (min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+var ObjectsData = {
+  NUMBERS_OBJECTS: 25,
+  MIN_LIKES: 15,
+  MAX_LIKES: 200,
+  MIN_AVATARS_COUNT: 1,
+  MAX_AVATARS_COUNT: 6,
+  MESSAGES: [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ],
+  USER_NAMES: ['Стас', 'Фёкла', 'Василий', 'Ашот', 'Лиза', 'Алексей']
 };
-/* Создаем массив однотипных обьектов */
-var getArrayOfPhoto = function (ObjectSample, count) {
-  for (var i = 0; i < count; i++) {
-    photosArray.push(new ObjectSample(i));
+
+// Функция, возвращает массив объектов записей
+function generateData() {
+  var data = [];
+  for (var i = 1; i < ObjectsData.NUMBERS_OBJECTS + 1; i++) {
+    data.push({
+      url: 'photos/' + i + '.jpg',
+      likes: getRandomNumber(ObjectsData.MIN_LIKES, ObjectsData.MAX_LIKES),
+      messages: generateMessages(),
+      description: getRandomElement(ObjectsData.MESSAGES)
+    });
   }
-  return photosArray;
-};
+  return data;
 
-/* ------------------------------------------------------------------------------------------------------ */
+  // Функция, возвращает массив объектов записей
+  function generateMessages() {
+    var messages = [];
 
-var getPhoto = function (k) {
-  var photo = {};
-  photo.url = 'photos/' + (k + 1) + '.jpg';
-  photo.likes = getRandomNumber(MIN_LIKES, MAX_LIKES);
-  var commentsUsers = getRandomNumber(MIN_COMMENTS, MAX_COMMENTS);
-  // photo.comments = [];
-  photo.comments = getRandomNumber(COMMENTS, commentsUsers);
-  photo.description = getRandomNumber(MESSAGE, DESCRIPTION_STRING);
+    var countComments = getRandomNumber(ObjectsData.MIN_AVATARS_COUNT, ObjectsData.MAX_AVATARS_COUNT - 1);
 
-  return photo;
-};
+    for (var j = 0; j < countComments; j++) {
+      messages.push({
+        avatar: generateSrcImage(ObjectsData.MIN_AVATARS_COUNT, ObjectsData.MAX_AVATARS_COUNT),
+        name: getRandomElement(ObjectsData.USER_NAMES),
+        message: getRandomElement(ObjectsData.MESSAGES)
+      });
+    }
+    return messages;
+  }
+  // Функция, возвращает url аватара
+  function generateSrcImage(min, max) {
+    return 'img/avatar-' + getRandomNumber(min, max) + '.svg';
+  }
 
-/* ------------------------------------------------------------------------------------------------------ */
+  // Функция, возвращает случайное число в диапазоне
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-
-var photoList = document.querySelector('.pictures');
-var photoTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
-
-var createPhotoElements = function (photoObject) {
-  var photoElement = photoTemplate.cloneNode(true);
-  photoElement.querySelector('.picture__img').src = photoObject.url;
-  photoElement.querySelector('.picture__likes').textContent = photoObject.likes;
-  photoElement.querySelector('.picture__comments').textContent = photoObject.comments;
-
-  return photoElement;
-};
-
-var photosFragment = document.createDocumentFragment();
-
-for (var i = 0; i < photosArray.length; i++) {
-  photosFragment.appendChild(createPhotoElements(photosArray[i]));
+  // Функция, возвращает случайный элемемент массива
+  function getRandomElement(array) {
+    var randomIndex = getRandomNumber(1, array.length - 1);
+    var randomElement = array[randomIndex];
+    return randomElement;
+  }
 }
 
-document.querySelector('.pictures').appendChild(photosFragment);
+var listData = generateData();
 
+// Галерея
+// Клонируем фотографии
+function renderDataList(arrayPictures) {
+  var picturesList = document.querySelector('.pictures'); // Ищем элемент в который мы будем вставлять наши изображения
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < arrayPictures.length; i++) {
+    fragment.appendChild(renderPicture(arrayPictures[i]));
+  }
+  picturesList.appendChild(fragment);
 
-/* ------------------------------------------------------------------------------------------------------ */
+  // Генерируем наш шаблон в документ
+  function renderPicture(image) {
+    var picturesTemplate = document.querySelector('#picture').content; // Ищем шаблон который мы будем копировать.
+    var picturesElement = picturesTemplate.cloneNode(true);
 
-var readyPhotos = getArrayOfPhoto(getPhoto, photoList, createPhotoElements, NUMBERS_OBJECTS);
-photosFragment(readyPhotos, photoList);
+    picturesElement.querySelector('.picture__img').src = image.url;
+    picturesElement.querySelector('.picture__likes').textContent = image.likes;
+    picturesElement.querySelector('.picture__comments').textContent = image.messages.length;
+    return picturesElement;
+  }
+}
+
+renderDataList(listData);
