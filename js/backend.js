60959285e76f587e847f2576fde39e649b3c0bd9
ÄleksRomanov <kosmonavt-vavilon@mail.ru>
2020-02-29 18:51:58 +0700

    6. Работа с сетью. 15. Личный проект: надо подкачаться (часть 1). Загрузка данных с сервера.

---
+++ js/backend.js
@@ -0,0 +1,40 @@
+'use strict';
+
+(function () {
+  var backend = {
+    load: function (onSuccess, onError) {
+      var URL = 'https://js.dump.academy/kekstagram/data';
+      createRequest('GET', URL, onSuccess, onError);
+    },
+
+    // save: function (data, onSuccess, onError) {
+    //   var URL = 'https://js.dump.academy/kekstagram';
+    //   createRequest('POST', URL, onSuccess, onError, data);
+    // },
+  };
+
+  var createRequest = function (method, url, onSuccess, onError, data) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+    xhr.timeout = 10000;
+    xhr.open(method, url);
+    xhr.send(data);
+  };
+
+
+  window.backend = backend;
+})();
